47b738db321a47b14f8b7b894e8ac07486a59976
JOTOKPA6728 <jotokpa6728@conestogac.on.ca>
2024-05-12 10:10:43 -0400

    editing the vscode file

--- students_submissions/8976728/vscode.py
+++ students_submissions/8976728/vscode.py
@@ -1,1 +1,8 @@
-2+4
+def two_numbers_equal(x,y):
+    ## TODO: Write a code that takes as an inputs two numbers (x,y) and returns True if x is equal to y and False otherwise
+    x= input("Enter value for x: ")
+    y= input("Enter value for y: ")
+    if x == y:
+       return True
+    else:
+       return False # Replace True by the needed return
